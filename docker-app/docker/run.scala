f81950739c728638b084a85ab3ea6303be0a516a
Samuel Cox <sacox@paypal.com>
2019-01-24 08:14:29 -0600

    Add options to produce JFR results.  Crappy storing of output..
    should be redone if we run this more than few times!

--- docker-app/docker/run.scala
+++ docker-app/docker/run.scala
@@ -2,17 +2,35 @@ import java.io.File
 
 import scala.sys.process._
 
-val usage = "Usage: (test --no-reporter (all | <client>) [<host> <port> <executions> <workers>]) | server | list | report <dir> | help"
+object Usage {
+  val commands = List(
+    "(test [--jfr] [--no-reporter] (all | <client>) [<host> <port> <executions> <workers>])",
+    "server",
+    "list",
+    "(report <dir>)",
+    "help"
+  )
+
+  override def toString = s"Usage: ${commands.mkString(" | ")}"
+}
 
 val suiteFileDir = "testng"
 
 // yah, some of this sucks, but I don't want to reach for a CLI-parser lib unless I revisit
 // this a few more times. (Even though you can make AWESOME CLIs with Scala:)
 
-val disableReporterFlag = "--no-reporter"
-val disableDropwizard = args.contains(disableReporterFlag)
+object Flags {
+  val disableReporter = "--no-reporter"
+  val enableJfr = "--jfr"
+  val flags = Set(disableReporter, enableJfr)
+}
 
-args.filter { _ != disableReporterFlag }.toList match {
+val disableDropwizard = args.contains(Flags.disableReporter)
+val enableJfr = args.contains(Flags.enableJfr)
+
+val argsWithoutFlags = args.filter { a => !Flags.flags.contains(a) }
+
+argsWithoutFlags.toList match {
   case List("test", "all", h, p, e, w) => runAllTests(h, p.toInt, Some(e.toInt), Some(w.toInt))
   case List("test", c    , h, p, e, w) => runTest(c, h, p.toInt, Some(e.toInt), Some(w.toInt))
   case List("test", "all")             => runAllTests("localhost", 8080)
@@ -57,14 +75,15 @@ def runTest(
   ).collect { case (n, Some(v)) => (n, v) }
     .map { case (n, v) => s"-Dbm.$n=$v" } 
 
+  val jfrStr = if (enableJfr) javaOptions(client) else ""
   val jarName = jarPath(s"${client}-benchmark")
-  val cmd = s"""java ${sysProps.mkString(" ")} -jar ${jarPath(s"$client-benchmark")} -usedefaultlisteners false $testFile"""
+  val cmd = s"""java $jfrStr ${sysProps.mkString(" ")} -jar ${jarPath(s"$client-benchmark")} -usedefaultlisteners false $testFile"""
   println(s"Executing:  $cmd")
   cmd.!
 }
 
 def exitWithUsage(): Unit = {
-  System.err.println(usage)
+  System.err.println(Usage)
   System.exit(1)
 }
 
@@ -80,4 +99,31 @@ def listClients(): Unit = clients().foreach { println }
 
 def runServer(): Unit = {
   s"""java -jar ${jarPath("mock-application")}""".run()
+}
+
+// Adopted from https://dzone.com/articles/using-java-flight-recorder-with-openjdk-11-2
+def javaOptions(client: String): String = {
+    List(
+      "-XX:+HeapDumpOnOutOfMemoryError",
+      s"-XX:HeapDumpPath=${dataFilename(s"$client-heapdump.hprof")}",
+      s"-XX:StartFlightRecording=${flightRecorderOptions(client)}"
+    ).mkString(" ")
+}
+
+def flightRecorderOptions(client: String): String = {
+  Map(
+    "disk"       -> "true",
+    "dumponexit" -> "true",
+    "filename"   -> dataFilename(s"$client-recording.jfr"),
+    "settings"   -> "default"
+  ).map { case (k, v) => s"$k=$v" }
+  .mkString(",")
+}
+
+def dataFilename(filename: String): String = {
+  // Yah, this human-knowledge coupling sucks:(:(
+  sys.env.get("BM.METRICS.DIR") match {
+    case Some(d) => s"$d/$filename"
+    case _       => filename
+  }
 }
